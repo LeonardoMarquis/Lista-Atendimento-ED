fed4fa493852d8fd6a93fbe2e9a5b79d147ce3bb
LeonardoMarquis <marquisleonardo12@gmail.com>
2025-09-06 15:40:28 -0300

    com saida sem fazer nada and match op

--- Cliente.py
+++ Cliente.py
@@ -11,41 +11,47 @@ def main():
         print("\n1)Chegada de uma pessoa para atendimento\n2)Atendimento de uma pessoa\n3)Listar pessoas\n0)Sair")
 
         op = int(input("\nOpcao: "))
-
-        if op == 1:
-            nome = str(input("\nNome: "))
-            tipo = str(input("\nNormal=2 | Prioridade=1\n"))
-            while tipo != "1" and tipo != "2":
+        
+        match op:
+            case 1:
+                nome = str(input("\nNome: "))
                 tipo = str(input("\nNormal=2 | Prioridade=1\n"))
+                while tipo != "1" and tipo != "2":
+                    tipo = str(input("\nNormal=2 | Prioridade=1\n"))
 
 
-            if tipo == "1":
-                l.inserir(nome, True)
+                if tipo == "1":
+                    l.inserir(nome, True)
 
-            elif tipo == "2":
-                l.inserir(nome, False)
-            else:
-                print("\nDigitaste algo errado! Recomece!")
+                elif tipo == "2":
+                    l.inserir(nome, False)
+                else:
+                    print("\nDigitaste algo errado! Recomece!")
 
-        elif op == 2:
-            l.atender()
+            case 2:
+                l.atender()
 
-        elif op == 3:
-            l.listar()
+            case 3:
+                l.listar()
 
 
-        elif op == 0:
-            #l.listar()
-            if l.zerada():
-                l.estatisticas_finais()
-                print("\nEncerrando.")
+            case 0:
+                if l.zerada() and l.total_atendidos_normal + l.total_atendidos_prioridade != 0:
+                    l.estatisticas_finais()
+                    print("\nEncerrando.")
 
-                break
-            else:
-                print("\nTermine todos os atendimentos antes de sair!")
+                    break
+                elif l.zerada() and l.total_atendidos_normal + l.total_atendidos_prioridade == 0:
+                    print("\nNenhuma operação realizada\nEncerrado")
+                    break
+                else:
+                    print("\nTermine todos os atendimentos antes de sair!")
 
-        else:
-            print("\nDigitaste alguma coisa errada!")
+            case _:
+                print("\nDigitaste alguma coisa errada!")
+
+
+
 
 
 if __name__ == "__main__":
